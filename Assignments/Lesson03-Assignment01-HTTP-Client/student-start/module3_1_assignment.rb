93ecba3e1ad94500725f6c9a2a9d0083a2d52095
WweiL <z920631580@gmail.com>
2018-07-07 15:40:38 +0800

    completed module3

--- Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
+++ Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
@@ -1,5 +1,15 @@
 require 'httparty'
 
-class Recipe
+ENV["FOOD2FORK_KEY"] = '939c9d386fae35b4a5db386fea105a24'
 
+class Recipe
+  include HTTParty
+  base_uri 'http://food2fork.com/api'
+  default_params key: ENV["FOOD2FORK_KEY"]
+  format :json
+
+  def self.for(term)
+    get("/search", query: {q: term})["recipes"]
+  end
+        
 end
